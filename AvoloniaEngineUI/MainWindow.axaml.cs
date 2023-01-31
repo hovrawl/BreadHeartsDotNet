9fa988d76cb67431989746f9d53f003422db8869
NahFarn <nathan.singh@inreachsw.com>
2023-01-31 19:58:55 +1100

    add avalonia engine UI

---
+++ AvoloniaEngineUI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvoloniaEngineUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
